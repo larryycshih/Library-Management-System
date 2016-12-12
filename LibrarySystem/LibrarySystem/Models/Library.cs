281cbba1da94196f51645e9be23534babf67ac70
Larry Shih <larryycshih@gmail.com>
2016-12-12 11:46:49 +1100

    updated the nuget packages

---
+++ LibrarySystem/LibrarySystem/Models/Library.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibrarySystem.Models
+{
+    public class Library
+    {
+        public int ID { get; set; }
+        public int TypeID { get; set; }
+        public int LanguageID { get; set; }
+        public int CategoryID { get; set; }
+        public String Name { get; set; }
+        public String Author { get; set; }
+        public String Publisher { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public Double Price { get; set; }
+        public String ISBN_ISSN { get; set; }
+        public String Comment { get; set; }
+    }
+}
---
+++ LibrarySystem/LibrarySystem/Models/Types.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibrarySystem.Models
+{
+    public class Types
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public String Comments { get; set; }
+    }
+}
